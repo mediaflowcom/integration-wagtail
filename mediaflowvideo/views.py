9d316dd82d0b1f4e07d68d6267119ab321973042
Jan <jan@mediaflow.com>
2024-03-26 09:04:55 +0100

    Localization for file selector

--- mediaflowvideo/views.py
+++ mediaflowvideo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from wagtail.admin.modal_workflow import render_modal_workflow
 from .models import MFVideoAppConfig
 from django.conf import settings
+from django.utils import translation
 # Create your views here.
 def mfvideo_modal(request):
 
@@ -9,10 +10,11 @@ def mfvideo_modal(request):
     client_id = getattr(settings, "MEDIAFLOW_CLIENT_ID", "")
     client_secret = getattr(settings, "MEDIAFLOW_CLIENT_SECRET", "")
     print("client_secet", client_secret)
+    
     return render_modal_workflow(
         request,
         'mediaflowvideo/mf-video-modal.html', # html template
         None,
-        {'trigger': request.GET.get('trigger'), "client_secret": client_secret, "client_id": client_id, "server_key": appSettings.server_key, "server_id": appSettings.server_id}, # html template vars
+        {'locale': translation.get_language(), 'trigger': request.GET.get('trigger'), "client_secret": client_secret, "client_id": client_id, "server_key": appSettings.server_key, "server_id": appSettings.server_id}, # html template vars
         json_data={'step': 'choose'}
     )
